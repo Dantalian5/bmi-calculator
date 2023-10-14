4c607eb3eabfb0f64d6fa9fef97c840652e11b4f
Marcos Ernesto Planos Valenzuela <87524702+Dantalian5@users.noreply.github.com>
2023-10-14 19:23:09 +0200

    dev bmi calc

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -6,20 +6,8 @@ const bmiDataHeightIn = document.getElementById("js-bmiDataHeightIn"); // height
 const bmiDataWeight = document.getElementById("js-bmiDataWeight"); // weight kg input.
 const bmiDataWeightLbs = document.getElementById("js-bmiDataWeightLbs"); // weight lbs input.
 let metricState = true; // metric system state - default true on metric.
-
-const metricSystem = document
-	.querySelectorAll("input[name=js-metric_system]")
-	.forEach((item) => {
-		item.addEventListener("change", (event) => {
-			if (item.value == "metric") {
-				metricStateSelection(true);
-				toggleMetricUnit(true);
-			} else if (item.value == "imperial") {
-				metricStateSelection(false);
-				toggleMetricUnit(false);
-			}
-		});
-	});
+const inputData = document.querySelectorAll(".js-inputData"); // input data
+const bmiResult = document.getElementById("js-bmiResult"); //html output
 
 function metricStateSelection(state) {
 	metricState = state;
@@ -38,3 +26,45 @@ function toggleMetricUnit(state) {
 		document.getElementById("js-weightLabel").innerText = "st";
 	}
 }
+function bmiOutput(res) {
+	let result;
+	let height1 = Array.from(inputData).filter((item) => {
+		return item.dataset.type === "height" && item.dataset.order === "1";
+	})[0].value;
+	let weight1 = Array.from(inputData).filter((item) => {
+		return item.dataset.type === "weight" && item.dataset.order === "1";
+	})[0].value;
+	if (height1 == "") {
+		height1 = 0;
+	}
+	if (weight1 == "") {
+		weight1 = 0;
+	}
+	result = weight1 / (height1 * 0.01) ** 2;
+	console.log(result);
+	bmiResult.value = result.toFixed(1);
+}
+
+const metricSystem = document
+	.querySelectorAll("input[name=js-metric_system]")
+	.forEach((item) => {
+		item.addEventListener("change", (event) => {
+			if (item.value == "metric") {
+				metricStateSelection(true);
+				toggleMetricUnit(true);
+			} else if (item.value == "imperial") {
+				metricStateSelection(false);
+				toggleMetricUnit(false);
+			}
+		});
+	});
+
+inputData.forEach((inputItem) => {
+	inputItem.addEventListener("input", (event) => {
+		if (inputItem.value != 0) {
+			bmiOutput(1);
+		} else {
+			bmiOutput(3);
+		}
+	});
+});
