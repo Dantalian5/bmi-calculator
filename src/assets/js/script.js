55907107dd1613f71048965a8a295a2aad9f6a0b
Marcos Ernesto Planos Valenzuela <87524702+Dantalian5@users.noreply.github.com>
2023-10-14 12:01:50 +0200

    dev jscript & fix states

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,19 +1,40 @@
-"use sctrict";
-const inputMetric = document.querySelectorAll(".js-metricInput");
+"use strict";
+const bmiCalculator = document.getElementById("js-bmiCalculator"); // The BMI calculator itself.
+const inputMetric = document.querySelectorAll(".js-metricInput"); // metric system selector input
+const bmiDataHeight = document.getElementById("js-bmiDataHeight"); // height cm input.
+const bmiDataHeightIn = document.getElementById("js-bmiDataHeightIn"); // height inches input.
+const bmiDataWeight = document.getElementById("js-bmiDataWeight"); // weight kg input.
+const bmiDataWeightLbs = document.getElementById("js-bmiDataWeightLbs"); // weight lbs input.
+let metricState = true; // metric system state - default true on metric.
+
 const metricSystem = document
 	.querySelectorAll("input[name=js-metric_system]")
 	.forEach((item) => {
-		item.addEventListener("click", (event) => {
+		item.addEventListener("change", (event) => {
 			if (item.value == "metric") {
-				inputMetric.forEach((item) => {
-					item.classList.add("js-metric");
-					item.classList.remove("js-imperial");
-				});
+				metricStateSelection(true);
+				toggleMetricUnit(true);
 			} else if (item.value == "imperial") {
-				inputMetric.forEach((item) => {
-					item.classList.remove("js-metric");
-					item.classList.add("js-imperial");
-				});
+				metricStateSelection(false);
+				toggleMetricUnit(false);
 			}
 		});
 	});
+
+function metricStateSelection(state) {
+	metricState = state;
+	if (!state) {
+		bmiCalculator.classList.add("js--imperial");
+	} else {
+		bmiCalculator.classList.remove("js--imperial");
+	}
+}
+function toggleMetricUnit(state) {
+	if (state) {
+		document.getElementById("js-heightLabel").innerText = "cm";
+		document.getElementById("js-weightLabel").innerText = "kg";
+	} else {
+		document.getElementById("js-heightLabel").innerText = "ft";
+		document.getElementById("js-weightLabel").innerText = "st";
+	}
+}
